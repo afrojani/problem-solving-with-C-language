f54bd5983177f86d78d5f864134368f9bf35bf14
afrojani <sultana.ani2019@gmail.com>
2023-02-13 03:03:18 -0600

    codeforces problem solution

---
+++ CodeForces Problem solving/team.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n, count = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        int p, v, t;
+
+        scanf("%d %d %d", &p, &v, &t);
+        if (p + v + t >= 2)
+            count++;
+    }
+    printf("%d\n", count);
+    return 0;
+}

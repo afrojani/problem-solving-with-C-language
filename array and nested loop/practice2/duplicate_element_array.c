dd0ba3c74b2c5129ba8f2f3886868d26a1626992
afrojani <sultana.ani2019@gmail.com>
2023-08-17 22:00:47 -0500

    counting sort

--- array and nested loop/practice2/duplicate_element_array.c
+++ array and nested loop/practice2/duplicate_element_array.c
@@ -4,12 +4,15 @@ int main()
     int n, i, j;
     scanf("%d", &n);
     int input[n];
+    // ekhane array input nise.
     for (i = 0; i < n; i += 1)
     {
         scanf("%d", &input[i]);
     }
+    // array er first element ta k largest dhore nilam.
     int largestElement = input[0];
 
+    // loop chalaye actual largest element ta ber korbo.
     for (i = 1; i < n; i += 1)
     {
         if (input[i] > largestElement)
@@ -17,19 +20,22 @@ int main()
             largestElement = input[i];
         }
     }
-
+    // largest element er cheye ek beshi ekta notun array declare korlam.
     int arr[largestElement + 1];
-
+    // notun array er sob value 0 kore nilam.
     for (i = 0; i <= largestElement; i += 1)
     {
         arr[i] = 0;
     }
-
+    /* main input array er proti ta element k index dhore new array te
+     oi index er against e 1 kore barabo.*/
     for (i = 0; i < n; i += 1)
     {
         arr[input[i]] += 1;
     }
 
+    /*new array te proti ta index e joto ta element hoise seta k j dhore loop ghuraye
+     print korlei sorted array chole asbe.*/
     for (i = 0; i <= largestElement /*&& i != 0*/; i += 1)
     {
         // printf("%d", arr[i]);
